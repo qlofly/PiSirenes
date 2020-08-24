7dde5dd34d57c459dc44161512351695d1ed7015
lofly <64271543+qlofly@users.noreply.github.com>
2020-08-24 12:55:45 +0300

    Add files via upload

---
+++ flsk.py
@@ -0,0 +1,18 @@
+import re
+import os
+import subprocess
+
+from multiprocessing import Value
+from subprocess import Popen, PIPE
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	uptim = subprocess.Popen(["awk", r'{printf("%d day(s) %02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}', "/proc/uptime"], stdout=subprocess.PIPE).communicate()
+	uptime = re.findall(r'\d\ \w\w\w\D\D\D \d\d:\d\d:\d\d', str(uptim))
+	templateData = {'timenow': uptime}
+
+	return render_template('index.html', **templateData)
