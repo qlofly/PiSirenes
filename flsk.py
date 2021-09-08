2b92eb31a2b42617a24fe0b215b373ca8dd915af
lofly <64271543+qlofly@users.noreply.github.com>
2021-09-08 23:36:19 +0300

    Update flsk.py

--- flsk.py
+++ flsk.py
@@ -2,17 +2,18 @@ import re
 import os
 import subprocess
 
+# from flask import request
 from multiprocessing import Value
 from subprocess import Popen, PIPE
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	uptim = subprocess.Popen(["awk", r'{printf("%d day(s) %02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}', "/proc/uptime"], stdout=subprocess.PIPE).communicate()
 	uptime = re.findall(r'\d\ \w\w\w\D\D\D \d\d:\d\d:\d\d', str(uptim))
-	templateData = {'timenow': uptime}
-
+	empty = ""
+	templateData = {'timenow': empty.join(uptime)}
 	return render_template('index.html', **templateData)
